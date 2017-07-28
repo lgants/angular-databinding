888d9f271861b3783dc6d1caa6ffa01810648874
lgants <lgants@gmail.com>
2017-07-28 18:40:44 -0500

    added an alias to binding

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
+
+// by default, all properties inside a component are not accessible from outside the component; so, need to use a decorator (e.g. @Input()) to allow parent components to be able to bind to a property
+
 export class ServerElementComponent implements OnInit {
+  // @Input() element: {type: string, name: string, content: string};
+  // @Input also accepts a property name to use as an alias 
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
